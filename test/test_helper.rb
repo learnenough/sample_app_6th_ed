82f6648560a60f851d56538d6e33ef68a5686e14
Michael Hartl <michael@michaelhartl.com>
2019-08-20 19:36:12 +0000

    Complete advanced testing setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
