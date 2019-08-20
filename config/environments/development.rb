2ecdcecdd0c9c15ddde671b2439240a540756016
Michael Hartl <michael@michaelhartl.com>
2019-08-20 18:01:07 +0000

    Allow connections to local server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Allow connections to local server.
+  config.hosts.clear
 end
