d61279c2044e9c857ec8307d7bae1aebc18e6c78
Michael Hartl <michael@michaelhartl.com>
2019-08-20 18:03:45 +0000

    Add a Static Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
