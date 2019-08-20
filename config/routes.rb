f5735b862e5ef09a900b5efb50175cc9c5edd20c
Michael Hartl <michael@michaelhartl.com>
2019-08-20 18:13:37 +0000

    Add hello to route route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'static_pages/home'
+  get 'static_pages/help'
+  root 'application#hello'
 end
