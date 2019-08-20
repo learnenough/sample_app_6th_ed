a54cfd9660e72cef6b976f23de8b40de35d67ee4
Michael Hartl <michael@michaelhartl.com>
2019-08-20 19:13:14 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
+  root 'static_pages#home'
+  get  'static_pages/home'
+  get  'static_pages/help'
   get  'static_pages/about'
-  root 'application#hello'
 end
