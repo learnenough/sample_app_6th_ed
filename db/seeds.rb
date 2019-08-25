cb007ce7706debcdc649cfc7e200c663b651bcc5
Michael Hartl <michael@michaelhartl.com>
2019-08-25 16:48:38 -0700

    Add comments to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
+# Create a main sample user.
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
              admin: true)
 
+# Generate a bunch of additional users.
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
