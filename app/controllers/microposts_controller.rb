d28563f504682f323044932adcd3abe77a58fb20
Michael Hartl <michael@michaelhartl.com>
2019-08-26 12:23:36 -0700

    Use AWS in production

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,7 +4,7 @@ class MicropostsController < ApplicationController
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
-    @micropost.picture.attach(params[:micropost][:picture])
+    @micropost.image.attach(params[:micropost][:image])
     if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to root_url
@@ -23,7 +23,7 @@ class MicropostsController < ApplicationController
   private
 
     def micropost_params
-      params.require(:micropost).permit(:content, :picture)
+      params.require(:micropost).permit(:content, :image)
     end
 
     def correct_user
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,12 +1,12 @@
 class Micropost < ApplicationRecord
   belongs_to       :user
-  has_one_attached :picture
-  # after_save       :resize_picture
+  has_one_attached :image
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 
-  def resize_picture
-    picture.variant(resize_to_limit: [500, 500])
+  # Returns a resized image for display.
+  def display_image
+    image.variant(resize_to_limit: [500, 500])
   end
 end
