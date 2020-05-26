8af4d6cc18875af3f9745d7b44048acd02c47cd3
Michael Hartl <michael@michaelhartl.com>
2020-05-26 10:44:57 -0700

    Add reset_session to guard against session fixation attacks

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,11 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       if user.activated?
+        forwarding_url = session[:forwarding_url]
+        reset_session
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_back_or user
+        redirect_to forwarding_url || user
       else
         message  = "Account not activated. "
         message += "Check your email for the activation link."
