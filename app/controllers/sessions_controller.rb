a462c066af468e0cbd48ed2f4f5660ee379d6a8d
Michael Hartl <michael@michaelhartl.com>
2020-05-26 10:45:26 -0700

    Add reset_session to guard against session fixation attacks

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      reset_session
       log_in user
       redirect_to user
     else
