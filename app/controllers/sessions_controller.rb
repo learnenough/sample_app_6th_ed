e3347f84c441e49dcb7062b3f7bd68285d6998e8
Michael Hartl <michael@michaelhartl.com>
2020-09-25 14:24:10 -0700

    Fix location of log_in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       forwarding_url = session[:forwarding_url]
       reset_session
-      log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      log_in user
       redirect_to forwarding_url || user
     else
       flash.now[:danger] = 'Invalid email/password combination'
