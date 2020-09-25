82c01cb6aed235d87aee7acecf1cbb5fc579ebc8
Michael Hartl <michael@michaelhartl.com>
2020-09-25 14:24:10 -0700

    Protect against session replay attacks

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       reset_session
-      log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      log_in user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
