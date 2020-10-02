0cdf6699437843b2f7504b29d7c4fb4dcea66bcd
seoul2045 <seoul1881@gmail.com>
2020-10-02 12:20:08 +0700

    Signup URL named route remains same after an instance of a signup error

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :users do
+  resources :users, path: '/signup' do
     member do
       get :following, :followers
     end
