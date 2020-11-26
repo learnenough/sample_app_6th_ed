e855f26b20b4f5cc9516c2d89e24b8be0672d41b
steven <wuqi0509@gmail.com>
2020-11-26 12:24:52 +0800

    Add FactoryBot methods support

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'support/factory_bot'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
