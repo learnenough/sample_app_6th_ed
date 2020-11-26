6d7886f08cd03f76a7e3946ba92510d6c87c5369
steven <wuqi0509@gmail.com>
2020-11-26 14:39:00 +0800

    Fix typo

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,7 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-# Add additional requires below this line. Rails is not loaded until this point!o
+# Add additional requires below this line. Rails is not loaded until this point!
 
 require 'support/factory_bot'
 
