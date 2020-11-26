8c73a926c618f98f89c13152c86603e703439c3b
steven <wuqi0509@gmail.com>
2020-11-26 14:18:08 +0800

    update rspec generator config

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,7 @@ module SampleApp
       g.test_framework :rspec,
         view_specs: false,
         helper_specs: false,
-        routing_specs: false,
-        request_specs: false
+        routing_specs: false
     end
   end
 end
