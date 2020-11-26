ded7ebbdc9369a487b3eb0e6de7ea095b6df7f3a
steven <wuqi0509@gmail.com>
2020-11-26 14:49:12 +0800

    Tweak

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
   describe '#new' do
     it 'returns a 200 response' do
-      get 'new'
-      expect(response).to be_success
+      get :new
+      expect(response.status).to eq 200
     end
   end
 end
