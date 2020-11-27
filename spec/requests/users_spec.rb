b6b430c05ab3d7f79d9da4f03fb5abfb60abc1f9
steven <wuqi0509@gmail.com>
2020-11-27 22:35:16 +0800

    Update user request specs

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,20 +3,73 @@ require 'rails_helper'
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
     context 'when user is signned in' do
+      subject { get users_path }
+
       before do
         @user = create(:user, activated: true, activated_at: Time.zone.now)
       end
 
       it 'retunrs a 200 response' do
         sign_in_as(@user)
-
-        get users_path
+        subject
         expect(response).to have_http_status(200)
       end
+
+      it 'render index template' do
+        sign_in_as(@user)
+        expect(subject).to render_template(:index)
+      end
     end
-    # it "works! (now write some real specs)" do
-    #   get users_index_path
-    #   expect(response).to have_http_status(200)
-    # end
+
+    context 'when user is not signned in' do
+      subject { get users_path }
+
+      it 'retunrs a 302 response' do
+        subject
+        expect(response).to have_http_status(302)
+      end
+
+      it 'render index template' do
+        expect(subject).to redirect_to(login_url)
+      end
+    end
+  end
+
+  describe "GET /users/:id/edit" do
+    context 'when user is signned in' do
+      before do
+        @user = create(:user, activated: true, activated_at: Time.zone.now)
+      end
+
+      subject { get edit_user_path(@user) }
+
+      it 'retunrs a 200 response' do
+        sign_in_as(@user)
+        subject
+        expect(response).to have_http_status(200)
+      end
+
+      it 'render edit template' do
+        sign_in_as(@user)
+        expect(subject).to render_template(:edit)
+      end
+    end
+
+    context 'when user is not signned in' do
+      before do
+        @user = create(:user, activated: true, activated_at: Time.zone.now)
+      end
+
+      subject { get edit_user_path(@user) }
+
+      it 'retunrs a 302 response' do
+        subject
+        expect(response).to have_http_status(302)
+      end
+
+      it 'render index template' do
+        expect(subject).to redirect_to(login_url)
+      end
+    end
   end
 end
