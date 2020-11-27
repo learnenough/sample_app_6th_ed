514ae2a69b9d420803162d87c9dd44cea0123fb0
steven <wuqi0509@gmail.com>
2020-11-28 00:31:47 +0800

    Load capybara to rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require'capybara/rspec'
 
 require 'support/factory_bot'
 require 'support/sign_in_helper'
