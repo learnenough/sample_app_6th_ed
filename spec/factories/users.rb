255bc4b07aa5b08fc3cc9e935999e978ca81c4ea
steven <wuqi0509@gmail.com>
2020-11-27 22:07:14 +0800

    Update user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :user do
     name  { Faker::Name.name }
     email { Faker::Internet.email }
-    password { User.digest('password') }
+    password { 'password' }
   end
 end
