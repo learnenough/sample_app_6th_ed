431604c01bf0ef6e9a6dc3e398bfb5e6145e2c2f
Michael Hartl <michael@michaelhartl.com>
2020-12-29 03:27:23 +0000

    Use SendGrid API key directly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,12 +72,12 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => 'apikey',
+    :password       => ENV['SENDGRID_API_KEY'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
-  
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
