e11920d4fcc63a9d31aea56c3e1b1cb0d992b20f
Michael Hartl <michael@michaelhartl.com>
2021-01-08 13:10:11 -0800

    Make minor changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,7 @@
 class User < ApplicationRecord
-  before_save { self.email = email.downcase }
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+                    uniqueness: { case_sensitive: false }
 end
