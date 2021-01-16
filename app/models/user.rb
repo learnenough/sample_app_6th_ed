0734aab80504949940af8046df7db544a263b0e6
Michael Hartl <michael@michaelhartl.com>
2021-01-16 07:34:25 -0800

    Fix regression in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
-  validates :name,  presence: true, length: { maximum: 50 }
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+                    uniqueness: true
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
