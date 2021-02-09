08709be3349c337e966c25bef666cb85ee312f63
Michael Hartl <michael@michaelhartl.com>
2021-02-09 12:43:33 -0800

    Improve session fixation solution

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -19,7 +19,9 @@ module SessionsHelper
   def current_user
     if (user_id = session[:user_id])
       user = User.find_by(id: user_id)
-      @current_user ||= user if session[:session_token] == user.session_token
+      if user && session[:session_token] == user.session_token
+        @current_user = user
+      end
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(:remember, cookies[:remember_token])
