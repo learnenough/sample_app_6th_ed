ecbd5fb1418130cc77b9d2cded9a158937dbeb1a
Michael Hartl <michael@michaelhartl.com>
2021-03-11 18:36:05 -0800

    Put imports in a more logical place

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "jquery"
+import "bootstrap"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-import "jquery"
-import "bootstrap"
