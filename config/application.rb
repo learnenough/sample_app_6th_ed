5a62770c66dbd62dd8caa253854eb0cfa4a206ab
Kevin Gilpin <kgilpin@gmail.com>
2021-05-17 14:58:40 -0400

    Remove server process recording

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,5 @@ module SampleApp
 
     # Include the authenticity token in remote forms.
     config.action_view.embed_authenticity_token_in_remote_forms = true
-
-    config.appmap.enabled = true if ENV['APPMAP_RECORD']
   end
 end
