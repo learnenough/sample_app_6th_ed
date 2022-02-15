1e2de2aff52437d0c375df0a4754b1f9c0ad348c
Michael Hartl <michael@michaelhartl.com>
2022-02-15 13:04:57 -0800

    Refactor password resets test

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -2,62 +2,82 @@ require 'test_helper'
 
 class PasswordResetsTest < ActionDispatch::IntegrationTest
 
-  def setup
-    ActionMailer::Base.deliveries.clear
-    @user = users(:michael)
+  class RestFormTest < ActionDispatch::IntegrationTest
+
+    def setup
+      @user = users(:michael)
+    end
+
+    test "invalid email" do
+      post password_resets_path, params: { password_reset: { email: "" } }
+      assert_not flash.empty?
+      assert_template 'password_resets/new'
+    end
+
+    test "valid email" do
+      post password_resets_path,
+           params: { password_reset: { email: @user.email } }
+      assert_not_equal @user.reset_digest, @user.reload.reset_digest
+      assert_equal 1, ActionMailer::Base.deliveries.size
+      assert_not flash.empty?
+      assert_redirected_to root_url
+    end
   end
 
-  test "password resets" do
-    get new_password_reset_path
-    assert_template 'password_resets/new'
-    assert_select 'input[name=?]', 'password_reset[email]'
-    # Invalid email
-    post password_resets_path, params: { password_reset: { email: "" } }
-    assert_not flash.empty?
-    assert_template 'password_resets/new'
-    # Valid email
-    post password_resets_path,
-         params: { password_reset: { email: @user.email } }
-    assert_not_equal @user.reset_digest, @user.reload.reset_digest
-    assert_equal 1, ActionMailer::Base.deliveries.size
-    assert_not flash.empty?
-    assert_redirected_to root_url
-    # Password reset form
-    user = assigns(:user)
-    # Wrong email
-    get edit_password_reset_path(user.reset_token, email: "")
-    assert_redirected_to root_url
-    # Inactive user
-    user.toggle!(:activated)
-    get edit_password_reset_path(user.reset_token, email: user.email)
-    assert_redirected_to root_url
-    user.toggle!(:activated)
-    # Right email, wrong token
-    get edit_password_reset_path('wrong token', email: user.email)
-    assert_redirected_to root_url
-    # Right email, right token
-    get edit_password_reset_path(user.reset_token, email: user.email)
-    assert_template 'password_resets/edit'
-    assert_select "input[name=email][type=hidden][value=?]", user.email
-    # Invalid password & confirmation
-    patch password_reset_path(user.reset_token),
-          params: { email: user.email,
-                    user: { password:              "foobaz",
-                            password_confirmation: "barquux" } }
-    assert_select 'div#error_explanation'
-    # Empty password
-    patch password_reset_path(user.reset_token),
-          params: { email: user.email,
-                    user: { password:              "",
-                            password_confirmation: "" } }
-    assert_select 'div#error_explanation'
-    # Valid password & confirmation
-    patch password_reset_path(user.reset_token),
-          params: { email: user.email,
-                    user: { password:              "foobaz",
-                            password_confirmation: "foobaz" } }
-    assert is_logged_in?
-    assert_not flash.empty?
-    assert_redirected_to user
+  class ResetSubmissionTest < ActionDispatch::IntegrationTest
+
+    def setup
+      post password_resets_path,
+           params: { password_reset: { email: users(:michael).email } }
+      @user = assigns(:user)
+    end
+
+    test "wrong email" do
+      get edit_password_reset_path(@user.reset_token, email: "")
+      assert_redirected_to root_url
+    end
+
+    test "inactive user" do
+      @user.toggle!(:activated)
+      get edit_password_reset_path(@user.reset_token, email: @user.email)
+      assert_redirected_to root_url
+    end
+
+    test "right email, wrong token" do
+      get edit_password_reset_path('wrong token', email: @user.email)
+      assert_redirected_to root_url
+    end
+
+    test "right email, right token" do
+      get edit_password_reset_path(@user.reset_token, email: @user.email)
+      assert_template 'password_resets/edit'
+      assert_select "input[name=email][type=hidden][value=?]", @user.email
+    end
+
+    test "invalid password & confirmation" do
+      patch password_reset_path(@user.reset_token),
+            params: { email: @user.email,
+                      user: { password:              "foobaz",
+                              password_confirmation: "barquux" } }
+      assert_select 'div#error_explanation'
+    end
+
+    test "empty password" do
+      patch password_reset_path(@user.reset_token),
+            params: { email: @user.email,
+                      user: { password:              "",
+                              password_confirmation: "" } }
+      assert_select 'div#error_explanation'
+    end
+
+    test "valid password & confirmation" do
+      patch password_reset_path(@user.reset_token),
+            params: { email: @user.email,
+                      user: { password:              "foobaz",
+                              password_confirmation: "foobaz" } }
+      assert is_logged_in?
+      assert_not flash.empty?
+      assert_redirected_to @user
+    end
   end
 end
