e6dfe2ea93c5683cde8cdb8af7557dfb4fc352ff
Michael Hartl <michael@michaelhartl.com>
2022-03-07 13:01:14 -0800

    Fix alignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,9 @@ User.create!(name:  "Example User",
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
-              email: email,
-              password:              password,
-              password_confirmation: password,
-              activated: true,
-              activated_at: Time.zone.now)
+               email: email,
+               password:              password,
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
