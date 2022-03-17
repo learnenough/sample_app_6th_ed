1fe102ea4f3c19444e3cb8278b309bd9cc428ef7
Kevin Gilpin <kgilpin@gmail.com>
2022-03-17 12:26:19 -0400

    fix: Refactor page_number into a function
    Ensure that page_number is always a number, preventing any malicious
    parameter injection.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   # to the user.
   def show
     @user = User.find(params[:id])
-    @microposts = @user.microposts.paginate(page: params[:page])
+    @microposts = @user.microposts.paginate(page: page_number)
   end
 
   # @label public
@@ -55,34 +55,39 @@ class UsersController < ApplicationController
   def following
     @title = "Following"
     @user  = User.find(params[:id])
-    @users = @user.following.paginate(page: params[:page])
+    @users = @user.following.paginate(page: page_number)
+
     render 'show_follow'
   end
 
   def followers
     @title = "Followers"
     @user  = User.find(params[:id])
-    @users = @user.followers.paginate(page: params[:page])
+    @users = @user.followers.paginate(page: page_number)
     render 'show_follow'
   end
 
-  private
+  protected
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
+  def page_number
+    Integer(params[:page] || 1)
+  end
 
-    # Before filters
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
+  end
 
-    # Confirms the correct user.
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
-    end
+  # Before filters
 
-    # Confirms an admin user.
-    def admin_user
-      redirect_to(root_url) unless current_user.admin?
-    end
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  # Confirms an admin user.
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
