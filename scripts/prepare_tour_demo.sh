a92ab2386e08c50cbbc40b47a72c7b71b1011c64
Kevin Gilpin <kgilpin@gmail.com>
2022-05-25 13:27:53 -0400

    fixup! feat: scripts/prepare_tour_demo.sh

--- scripts/prepare_tour_demo.sh
+++ scripts/prepare_tour_demo.sh
@@ -2,12 +2,7 @@
 
 set -ex
 
-git checkout HEAD Gemfile Gemfile.lock appmap.yml
-
-bundle
-APPMAP=true DISABLE_SPRING=true bundle exec rake
-yarn run openapi > swagger/openapi_stable.yaml
-
+git checkout HEAD app test Gemfile Gemfile.lock appmap.yml swagger/openapi_stable.yaml
 rm -f ~/.appmaprc
 rm appmap.yml
 cat scripts/remove_appmap_gem.patch | git apply
