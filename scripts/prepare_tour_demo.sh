cbfdcfb02ef3ad397bd43e30cd8d9820bf3f49ee
Kevin Gilpin <kgilpin@gmail.com>
2022-05-25 10:54:20 -0400

    fixup! feat: scripts/prepare_tour_demo.sh

--- scripts/prepare_tour_demo.sh
+++ scripts/prepare_tour_demo.sh
@@ -2,6 +2,7 @@
 
 set -ex
 
-rm ~/.appmaprc
+git checkout HEAD Gemfile Gemfile.lock appmap.yml
+rm -f ~/.appmaprc
 rm appmap.yml
-cat scripts/remove_appmap_gem.patch | git apply || true
+cat scripts/remove_appmap_gem.patch | git apply
