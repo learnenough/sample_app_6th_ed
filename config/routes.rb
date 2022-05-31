bfd4a58139b25059a41b51217d344040c5419e3b
Michael Hartl <michael@michaelhartl.com>
2022-05-30 20:36:55 -0700

    Make minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,6 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
-  get '/microposts', to: 'static_pages#home'
   resources :relationships,       only: [:create, :destroy]
+  get '/microposts', to: 'static_pages#home'
 end
