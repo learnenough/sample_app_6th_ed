064396133253e4084f964b88a0a5f4672fe27e90
Pete Cheslock <petecheslock@gmail.com>
2022-06-17 10:32:18 -0400

    Update devcontainer

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,7 +1,9 @@
-rvm install "ruby-3.0.2"
-rvm use 3.0.2
 bundle install
 yarn install
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+sudo apt-get update
+sudo apt-get install redis
 bundle exec rails webpacker:install
 bundle exec rails webpacker:compile
 bundle exec rails db:migrate
