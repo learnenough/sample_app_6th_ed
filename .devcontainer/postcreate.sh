ffc0fab54430cace6ca02488bd74dcdfaf4f90d0
Pete Cheslock <petecheslock@gmail.com>
2022-06-17 10:54:34 -0400

    Update Devcontainer

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,9 +1,9 @@
 bundle install
-yarn install
 curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
-sudo apt-get update
-sudo apt-get install redis
+sudo apt-get -y update
+sudo apt-get -y install redis python2 graphviz
+yarn install --check-files
 bundle exec rails webpacker:install
 bundle exec rails webpacker:compile
 bundle exec rails db:migrate
