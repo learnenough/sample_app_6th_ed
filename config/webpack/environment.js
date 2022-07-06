ac977a3af2d85ad8bc4de6d71dcf90b045f25921
Pete Cheslock <petecheslock@gmail.com>
2022-07-06 14:17:41 -0400

    Add webpack and jquery configuration

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
+  $: 'jquery/src/jquery',
+  jQuery: 'jquery/src/jquery',
+  jquery: 'jquery',
+  'window.jQuery': 'jquery',
+  Popper: ['popper.js', 'default']
+}))
+
 module.exports = environment
