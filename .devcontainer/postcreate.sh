0350185c0e50c84c37e78c7d29e5e8c0a3b603a7
Pete Cheslock <petecheslock@gmail.com>
2022-07-20 15:28:53 -0400

    Update devcontainer

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -3,7 +3,7 @@ curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyr
 echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
 sudo apt-get -y update
 sudo apt-get -y install redis python2 graphviz
+bundle install --without production
 yarn install --check-files
-bundle exec rails webpacker:install
-bundle exec rails webpacker:compile
 bundle exec rails db:migrate
+bundle exec rake db:seed
